7086ecb2790078e0e654f3071dc27d7037625786
Wesley <wesleyalkemade@outlook.com>
2018-10-02 21:47:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+To use the interface, download the file `referee-interface-win32-x64.rar` and extract it. Then open the folder, and open the file `referee-interface.exe` and you're good to go!
+
+To reset your password go to the `Settings` tab to forget the IRC credentials.
+
+I've used the following module to package the files: https://github.com/electron-userland/electron-packager in case you want to package it yourself :)
+
+![](https://i.imgur.com/IWSyFOZ.png)
+![](https://i.imgur.com/AmpTjYq.png)
+![](https://i.imgur.com/H7qVdRL.png)
+![](https://i.imgur.com/yTI8gfZ.png)
---
+++ index.js
@@ -0,0 +1,70 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path              = require('path');
+const url               = require('url');
+const Store             = require('electron-store');
+
+const store = new Store();
+
+// ============================
+// Create some global variables
+let mainWindow;
+
+
+initialize = () => {
+
+}
+
+// ==================
+// Initialize the app
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({icon: __dirname + '/web/resources/favicon.png', backgroundColor: '#3C4452'}); 
+
+    mainWindow.on('close', () => {
+        mainWindow = null;
+        app.quit();
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'web/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.maximize();
+
+    initialize();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        initialize();
+    }
+});
+
+// ==================================================
+// Prevent the app from crashing when an error occurs
+process.on("uncaughtException", err => {
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    console.error("Uncaught Exception: ", errorMsg);
+    
+    allErrors.push({
+        type: 'uncaughtException',
+        error: JSON.stringify(err)
+    });
+});
+
+process.on("unhandledRejection", err => {
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    console.error("Uncaught Promise Error: ", errorMsg);
+
+    allErrors.push({
+        type: 'unhandledRejection',
+        error: JSON.stringify(err)
+    });
+});
