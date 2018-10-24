c792a9a20b5a87f18203097b65d8b1d825b63f79
Wesley Alkemade <wesleyalkemade@outlook.com>
2018-10-24 23:53:04 +0200

    Calculating and showing the scores

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const store = new Store();
 let mainWindow;
 
 
-initialize = () => {
+initialize = async () => {
+    const cache = store.get('cache');
 
+    // Create the cache if it doesn't exist
+    if(cache == undefined)
+        await store.set('cache', {"beatmaps": {}, "users": {}});
 }
 
 // ==================
@@ -70,7 +74,7 @@ ipcMain.on('requestAPIKey', (event, arg) => {
 
 // ===========================================
 // This will be called when you create a lobby
-ipcMain.on('createLobby', (event, arg) => {
+ipcMain.on('createLobby', async (event, arg) => {
     const lobbyToken = fnc.createToken(10);
 
     // Store the settings
@@ -80,6 +84,86 @@ ipcMain.on('createLobby', (event, arg) => {
         teamOneName:        arg['team-one-name'],
         teamTwoName:        arg['team-two-name']
     });
+
+    const apiKey        = store.get('api-key');
+    let cache         = store.get('cache');
+    const multiplayerId = fnc.getMultiplayerIdFromUrl(arg['match-url']);
+
+    await rp(`https://osu.ppy.sh/api/get_match?k=${apiKey}&mp=${multiplayerId}`).then(async multiplayerLobby => {
+        multiplayerLobby = JSON.parse(multiplayerLobby);
+
+        // Loop through all the games
+        for(let game in multiplayerLobby.games) {
+            const currentGame = multiplayerLobby.games[game];
+
+            let gameScore = {};
+
+            // Loop through all the scores of the current map
+            for(let score in currentGame.scores) {
+                const currentScore = currentGame.scores[score];
+
+                // Check if the user has been cached, if not cache it
+                if(!cache.users.hasOwnProperty(currentScore.user_id)) {
+                    await rp(`https://osu.ppy.sh/api/get_user?k=${apiKey}&u=${currentScore.user_id}`).then(user => {
+                        user = JSON.parse(user);
+                        
+                        cache.users[currentScore.user_id] = user[0].username;
+                        store.set(`cache.users.${currentScore.user_id}`, user[0].username);
+                    });
+
+                    console.log(`user ${currentScore.user_id} not cached`);
+                }
+
+                // Accuracy players
+                if(['0','3'].indexOf(currentScore.slot) > -1) {
+                    gameScore.user = currentScore.user_id;
+                    gameScore.score = (currentScore.pass == 0 ? 0 : fnc.calculateAccuracyPlayerScore(currentScore.score));
+                    gameScore.accuracy = (currentScore.pass == 0 ? 0 : fnc.getAccuracyOfScore(currentScore));
+                    gameScore.passed = currentScore.pass;
+
+                    store.set(`lobby.${lobbyToken}.multiplayerData.${currentGame.game_id}.${currentScore.slot}`, gameScore);
+                }
+                // Score players
+                else if(['1','2','4','5'].indexOf(currentScore.slot) > -1) {
+                    gameScore.user = currentScore.user_id;
+                    gameScore.score = (currentScore.pass == 0 ? 0 : fnc.calculateScorePlayerScore(currentScore.score, fnc.getAccuracyOfScore(currentScore), 7));
+                    gameScore.accuracy = (currentScore.pass == 0 ? 0 : fnc.getAccuracyOfScore(currentScore));
+                    gameScore.passed = currentScore.pass;
+
+                    store.set(`lobby.${lobbyToken}.multiplayerData.${currentGame.game_id}.${currentScore.slot}`, gameScore);
+                }
+
+                store.set(`lobby.${lobbyToken}.multiplayerData.${currentGame.game_id}.beatmap_id`, currentGame.beatmap_id);
+            }
+
+            // Check if the beatmap has been cached, if not cache it
+            if(!cache.beatmaps.hasOwnProperty(currentGame.beatmap_id)) {
+                await rp(`https://osu.ppy.sh/api/get_beatmaps?k=${apiKey}&b=${currentGame.beatmap_id}&m=2&a=1`).then(beatmap => {
+                    beatmap = JSON.parse(beatmap);
+
+                    store.set(`cache.beatmaps.${currentGame.beatmap_id}`, {
+                        name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                        beatmapset_id: beatmap[0].beatmapset_id
+                    });
+                });
+
+                console.log(`beatmap ${currentGame.beatmap_id} not cached`);
+            }
+        }
+    }).finally(() => {
+        const multiplayerGames = store.get(`lobby.${lobbyToken}.multiplayerData`);
+
+        // Loop through the games
+        for(let game in multiplayerGames) {
+            const currentGame = multiplayerGames[game];
+
+            const finalTeamOneScore = fnc.calculateTeamScore(currentGame[0].score, currentGame[1].score, currentGame[2].score, currentGame[0].accuracy, 7);
+            const finalTeamTwoScore = fnc.calculateTeamScore(currentGame[3].score, currentGame[4].score, currentGame[5].score, currentGame[3].accuracy, 7);
+
+            store.set(`lobby.${lobbyToken}.multiplayerData.${game}.team_one_score`, finalTeamOneScore);
+            store.set(`lobby.${lobbyToken}.multiplayerData.${game}.team_two_score`, finalTeamTwoScore);
+        }
+    });;
 
     // ==========================================================
     // Send a message to frontend that the match has been created
@@ -112,39 +196,90 @@ ipcMain.on('requestMultiplayerLobby', (event, lobbyId) => {
 ipcMain.on('retrieveMultiplayerData', async (event, lobbyId) => {
     const apiKey    = store.get('api-key');
     const lobby     = store.get(`lobby.${lobbyId}`);
+    let cache       = store.get(`cache`);
 
     // Check if the lobby exists
     if(lobby) {
         const multiplayerId = fnc.getMultiplayerIdFromUrl(lobby.data.multiplayerLink);
 
-        await rp(`https://osu.ppy.sh/api/get_match?k=${apiKey}&mp=${multiplayerId}`).then(multiplayerLobby => {
+        await rp(`https://osu.ppy.sh/api/get_match?k=${apiKey}&mp=${multiplayerId}`).then(async multiplayerLobby => {
             multiplayerLobby = JSON.parse(multiplayerLobby);
 
+            // Loop through all the games
             for(let game in multiplayerLobby.games) {
-                store.set(`lobby.${lobbyId}.multiplayerData.${multiplayerLobby.games[game].game_id}`, multiplayerLobby.games[game]);
+                const currentGame = multiplayerLobby.games[game];
+
+                let gameScore = {};
+
+                // Loop through all the scores of the current map
+                for(let score in currentGame.scores) {
+                    const currentScore = currentGame.scores[score];
+
+                    // Check if the user has been cached, if not cache it
+                    if(!cache.users.hasOwnProperty(currentScore.user_id)) {
+                        await rp(`https://osu.ppy.sh/api/get_user?k=${apiKey}&u=${currentScore.user_id}`).then(user => {
+                            user = JSON.parse(user);
+                            
+                            cache.users[currentScore.user_id] = user[0].username;
+                            store.set(`cache.users.${currentScore.user_id}`, user.username);
+                        });
+                    }
+
+                    // Accuracy players
+                    if(['0','3'].indexOf(currentScore.slot) > -1) {
+                        gameScore.user = currentScore.user_id;
+                        gameScore.score = (currentScore.pass == 0 ? 0 : fnc.calculateAccuracyPlayerScore(currentScore.score));
+                        gameScore.accuracy = (currentScore.pass == 0 ? 0 : fnc.getAccuracyOfScore(currentScore));
+                        gameScore.passed = currentScore.pass;
+
+                        store.set(`lobby.${lobbyId}.multiplayerData.${currentGame.game_id}.${currentScore.slot}`, gameScore);
+                    }
+                    // Score players
+                    else if(['1','2','4','5'].indexOf(currentScore.slot) > -1) {
+                        gameScore.user = currentScore.user_id;
+                        gameScore.score = (currentScore.pass == 0 ? 0 : fnc.calculateScorePlayerScore(currentScore.score, fnc.getAccuracyOfScore(currentScore), 7));
+                        gameScore.accuracy = (currentScore.pass == 0 ? 0 : fnc.getAccuracyOfScore(currentScore));
+                        gameScore.passed = currentScore.pass;
+
+                        store.set(`lobby.${lobbyId}.multiplayerData.${currentGame.game_id}.${currentScore.slot}`, gameScore);
+                    }
+
+                    store.set(`lobby.${lobbyId}.multiplayerData.${currentGame.game_id}.beatmap_id`, currentGame.beatmap_id);
+                }
+
+                // Check if the beatmap has been cached, if not cache it
+                if(!cache.beatmaps.hasOwnProperty(currentGame.beatmap_id)) {
+                    await rp(`https://osu.ppy.sh/api/get_beatmaps?k=${apiKey}&b=${currentGame.beatmap_id}&m=2&a=1`).then(beatmap => {
+                        beatmap = JSON.parse(beatmap);
+
+                        cache.beatmaps[currentGame.beatmap_id] = {
+                            name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                            beatmapset_id: beatmap[0].beatmapset_id
+                        };
+
+                        store.set(`cache.beatmaps.${currentGame.beatmap_id}`, {
+                            name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                            beatmapset_id: beatmap[0].beatmapset_id
+                        });
+                    });
+                }
+            }
+        }).finally(() => {
+            const multiplayerGames = store.get(`lobby.${lobbyId}.multiplayerData`);
+
+            // Loop through the games
+            for(let game in multiplayerGames) {
+                const currentGame = multiplayerGames[game];
+
+                const finalTeamOneScore = fnc.calculateTeamScore(currentGame[0].score, currentGame[1].score, currentGame[2].score, currentGame[0].accuracy, 7);
+                const finalTeamTwoScore = fnc.calculateTeamScore(currentGame[3].score, currentGame[4].score, currentGame[5].score, currentGame[3].accuracy, 7);
+
+                store.set(`lobby.${lobbyId}.multiplayerData.${game}.team_one_score`, finalTeamOneScore);
+                store.set(`lobby.${lobbyId}.multiplayerData.${game}.team_two_score`, finalTeamTwoScore);
             }
         });
-
-        const allMaps = store.get(`lobby.${lobbyId}.multiplayerData`);
-        let beatmapCache = store.get(`cache.beatmaps`);
-
-        for(let map in allMaps) {
-            const currentMap = allMaps[map];
-            const currentBeatmapId = currentMap.beatmap_id;
-
-            if(beatmapCache == undefined || !beatmapCache.hasOwnProperty(currentBeatmapId)) {
-                await rp(`https://osu.ppy.sh/api/get_beatmaps?k=${apiKey}&b=${currentBeatmapId}&m=2&a=1`).then(beatmap => {
-                    beatmap = JSON.parse(beatmap);
-
-                    store.set(`cache.beatmaps.${currentBeatmapId}`, {
-                        name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
-                        beatmapset_id: beatmap[0].beatmapset_id
-                    });
-                });
-            }
-        }
     }
-}); 
+});
 
 // ==================================================
 // Prevent the app from crashing when an error occurs
