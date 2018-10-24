7ea430fa48f85f1fdad42f0f72cf1e9c2bc8f1eb
Wesley Alkemade <wesleyalkemade@outlook.com>
2018-10-24 23:52:50 +0200

    Functions to calculate the scores

--- plugins/functions.js
+++ plugins/functions.js
@@ -17,5 +17,36 @@ module.exports = {
         }
 
         return false;
+    },
+    getAccuracyOfScore: function(score) {
+        return (((parseInt(score.count50) + parseInt(score.count100) + parseInt(score.count300)) / (parseInt(score.count50) + parseInt(score.count100) + parseInt(score.count300) + parseInt(score.countmiss) + parseInt(score.countkatu))) * 100).toFixed(2);
+    },
+    addDot: function(nStr, splitter) {
+        nStr += '';
+        const x = nStr.split('.');
+        const x1 = x[0];
+        const x2 = x.length > 1 ? '.' + x[1] : '';
+        const rgx = /(\d+)(\d{3})/;
+
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + splitter + '$2');
+        }
+
+        return x1 + x2;
+    },
+    calculateAccuracyPlayerScore: function(score) {
+        return (score * 0.2);
+    },
+    calculateScorePlayerScore: function(score, accuracy, modifier) {
+        return (score * (Math.pow((100 - ((100 - accuracy) / 5)) / 100, modifier)));
+    },
+    calculateTeamScore: function(score_player_one, score_player_two, score_player_three, accuracy_player_one, modifier) {
+        score_player_one = parseFloat(score_player_one);
+        score_player_two = parseFloat(score_player_two);
+        score_player_three = parseFloat(score_player_three);
+
+        accuracy_player_one = parseFloat(accuracy_player_one);
+
+        return (((score_player_one) + (score_player_two) + (score_player_three)) * (Math.pow(accuracy_player_one / 100, modifier))).toFixed();
     }
 };
