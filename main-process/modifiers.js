f1251828c2f68a885d9795871e04342b576bbbab
Wesley Alkemade <wesleyalkemade@outlook.com>
2018-11-17 15:50:25 +0100

    Modifier page

---
+++ main-process/modifiers.js
@@ -0,0 +1,166 @@
+const {app, ipcMain, dialog} = require('electron');
+const fs = require('fs');
+const fnc = require('../plugins/functions.js');
+const rp = require('request-promise');
+
+const Bitbucket = require('bitbucket');
+const bitbucket = new Bitbucket();
+
+const Store = require('electron-store');
+const store = new Store();
+
+// ======================================
+// This is called when accessing any page
+ipcMain.on('checkVersion', (event, arg) => {
+    // =====================================================================
+    // Check if the version is the latest version, if not send out a warning
+    bitbucket.repositories.getSrc({
+        'username': 'wesley221',
+        'repo_slug': 'axs-calculator',
+        'node': 'master',
+        'path': 'package.json'
+    }).then(({data, headers}) => {
+        data = JSON.parse(data);
+
+        const   currentVersion = app.getVersion()
+                remoteVersion = data.version;        
+        
+        // ==================
+        // The version is old
+        if(currentVersion < remoteVersion) {
+            event.sender.send('oldVersion', {
+                'currentVersion': currentVersion,
+                'latestVersion': remoteVersion
+            });
+        }
+    });
+});
+
+// ========================================================
+// This is called on the pages where an API key is required
+ipcMain.on('requestApiValidation', (event, arg) => {
+    const apiKeyValid = store.get('api-key.valid');
+
+    // Send back the state of the api key to the front end
+    event.sender.send(`onRequestedApiValidation`, apiKeyValid);
+});
+
+// ==================================================
+// This is called when a user loads the modifier page
+ipcMain.on('requestAllModifiers', (event, arg) => {
+    const allModifiers = store.get('cache.modifiers');
+
+    // Send back a response to the front end
+    event.sender.send('requestedModifiers', allModifiers);
+});
+
+// =======================================
+// This is called when importing modifiers
+ipcMain.on('importModifiers', (event, arg) => {
+    // =======================
+    // Show a file open window
+    dialog.showOpenDialog({
+        'title': 'Import modifiers',
+        'defaultPath': 'modifiers.json',
+        'filters': [
+            {name: 'json', extensions: ['json']}
+        ]
+    }, fileLocation => {
+        // =====================================
+        // Check if a location has been selected
+        if(fileLocation != undefined) {
+            fileLocation = fileLocation[0];
+
+            // ======================
+            // Read the selected file
+            fs.readFile(fileLocation, 'utf-8', (err, data) => {
+                // ================
+                // Check for errors
+                if(err) {
+                    dialog.showErrorBox('Error!', `There was an error trying to import the file: ${err.message}.`);
+                }
+                else {
+                    data = JSON.parse(data);
+
+                    // Loop through the beatmaps
+                    for(let beatmap in data) {
+                        // Check for errors
+                        if(isNaN(beatmap)) {
+                            dialog.showErrorBox('Error!', `The format of the imported file is correct.`);
+                            return;
+                        }
+
+                        // Check for errors
+                        if(!data[beatmap].hasOwnProperty('beatmap_name') || !data[beatmap].hasOwnProperty('beatmap_id') || !data[beatmap].hasOwnProperty('modifier')) {
+                            dialog.showErrorBox('Error!', `The format of the imported file is correct.`);
+                            return;
+                        }
+
+                        // Save the modifier
+                        store.set(`cache.modifiers.${beatmap}`, data[beatmap]);
+                    }
+
+                    // ================
+                    // Show message box
+                    dialog.showMessageBox({
+                        'type': 'info',
+                        'message': 'Successfully imported the modifiers.'
+                    });
+
+                    // Send message when modifiers have been imported
+                    event.sender.send('modifiersImported', true);
+                }
+            });
+        }
+    });
+});
+
+// =========================================================
+// This is called when a user succesfully creates a modifier
+ipcMain.on('createBeatmapModifier', async (event, arg) => {
+    const apiKey    = store.get('api-key.key');
+    const beatmapId = fnc.getBeatmapIdFromUrl(arg.beatmapUrl);
+    let cache       = store.get('cache');
+
+    // Check if the beatmap was already cached
+    if(cache.beatmaps.hasOwnProperty(beatmapId)) {
+        store.set(`cache.modifiers.${beatmapId}`, {
+            'beatmap_name': cache.beatmaps[beatmapId].name,
+            'beatmap_id': beatmapId,
+            'modifier': arg.modifier
+        });
+    }
+    else {
+        rp(`https://osu.ppy.sh/api/get_beatmaps?k=${apiKey}&b=${beatmapId}&m=2&a=1`).then(beatmap => {
+            beatmap = JSON.parse(beatmap);
+            
+            cache.beatmaps[beatmapId] = {
+                name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                beatmapset_id: beatmap[0].beatmapset_id
+            };
+
+            store.set(`cache.beatmaps.${beatmapId}`, {
+                name: `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                beatmapset_id: beatmap[0].beatmapset_id
+            });
+
+            store.set(`cache.modifiers.${beatmapId}`, {
+                'beatmap_name': `${beatmap[0].artist} - ${beatmap[0].title} [${beatmap[0].version}]`,
+                'beatmap_id': beatmapId,
+                'modifier': arg.modifier
+            });
+        });   
+    }
+
+    // Send back a responserequestedLobby to the front end 
+    event.sender.send('modifierCreated', arg.beatmapUrl);
+});
+
+// =======================================
+// This is called when deleting a modifier
+ipcMain.on('deleteModifier', (event, arg) => {
+    store.delete(`cache.modifiers.${arg}`);
+
+    // Send a response to the front end
+    event.sender.send('deletedModifier', arg);
+});
---
+++ renderer-process/modifiers.js
@@ -0,0 +1,122 @@
+const {ipcRenderer} = require('electron');
+
+$(() => {
+    // Request if the api key is valid
+    ipcRenderer.send('requestApiValidation', true);
+
+    // The returned state of the api key
+    ipcRenderer.on('onRequestedApiValidation', (event, arg) => {
+        if(arg == false) {
+            $('body').append(`<div class="alert alert-danger apiError"><h2><i class="fas fa-exclamation-triangle"></i> Error</h2>You have not set your API key yet. Make sure to set your API key <a href="./settings.html">here</a>.</div>`);
+        }
+    });
+
+    // Request the modifiers from the cache
+    ipcRenderer.send('requestAllModifiers', true);
+
+    ipcRenderer.on('requestedModifiers', (event, arg) => {
+        for(let beatmap in arg) {
+            const currentModifier = arg[beatmap];
+
+            $('#allModifiers').append(`<tr id=${currentModifier.beatmap_id}>
+                                        <td>${currentModifier.beatmap_name}</td>
+                                        <td>
+                                            <input id="beatmap_${currentModifier.beatmap_id}" type=text class=form-control placeholder="Beatmap url" value="${currentModifier.beatmap_id}" />
+                                        </td>
+
+                                        <td>
+                                            <input id="modifier_${currentModifier.beatmap_id}" type=text class=form-control placeholder="Modifier" value="${currentModifier.modifier}" />
+                                        </td>
+
+                                        <td>
+                                            <button type=button id="save_${currentModifier.beatmap_id}" class="btn btn-sb-menu btn-xs" title="Save"><i class="far fa-save"></i></button>
+                                            <button type=button id="remove_${currentModifier.beatmap_id}" class="btn btn-sb-menu btn-xs" title="Remove"><i class="far fa-trash-alt"></i></button>
+                                            <span class=red-text id=error_${currentModifier.beatmap_id}></span>
+                                            <span class=green-text id=message_${currentModifier.beatmap_id}></span>
+                                        </td>
+                                    </tr>`);
+        }
+    });
+
+    $('#createNewModifier').on('click', function() {
+        const randomToken = generateToken(10);
+
+        $('#allModifiers').append(`<tr id=${randomToken}>
+                                        <td></td>
+                                        <td>
+                                            <input id="beatmap_${randomToken}" type=text class=form-control placeholder="Beatmap url" />
+                                        </td>
+
+                                        <td>
+                                            <input id="modifier_${randomToken}" type=text class=form-control placeholder="Modifier" />
+                                        </td>
+
+                                        <td>
+                                            <button type=button id="save_${randomToken}" class="btn btn-sb-menu btn-xs" title="Save"><i class="far fa-save"></i></button>
+                                            <button type=button id="remove_${randomToken}" class="btn btn-sb-menu btn-xs" title="Remove"><i class="far fa-trash-alt"></i></button>
+                                            <span class=red-text id=error_${randomToken}>This modifier has not been saved yet.</span>
+                                            <span class=green-text id=message_${randomToken}></span>
+                                        </td>
+                                    </tr>`);
+    });
+
+    ipcRenderer.on('deletedModifier', (event, arg) => {
+        $(`#${arg}`).remove();
+    });
+
+    // =====================================
+    // Send out to check the current version
+    ipcRenderer.send('checkVersion', true);
+
+    // ==============================================
+    // Check if the application is the latest version
+    ipcRenderer.on('oldVersion', (event, arg) => {
+        $('#cur-version').html(arg.currentVersion);
+        $('#latest-version').html(arg.latestVersion);
+
+        $('.sb-old-version').addClass('in');
+    });
+
+    $('#importModifiers').on('click', function() {
+        // ======================================
+        // Send out a request to import modifiers
+        ipcRenderer.send('importModifiers', true);
+    });
+
+    // ==============================================
+    // Modifiers have been imported, refresh the page
+    ipcRenderer.on('modifiersImported', (event, arg) => {
+        window.location.reload();
+    });
+});
+
+$('#allModifiers').on('click', '[id^=save_]', function() {
+    const thisId = this.id.replace('save_', '');
+    const beatmapUrl = $(`#beatmap_${thisId}`).val();
+    const modifier  = $(`#modifier_${thisId}`).val();
+
+    if(!isValidBeatmapUrl(beatmapUrl)) {
+        $(`#error_${thisId}`).html('Invalid beatmap url');
+    }
+    else if(!$.isNumeric(modifier)) {
+        $(`#error_${thisId}`).html('Invalid modifier');
+    }
+    else {
+        $(`#error_${thisId}`).html('');
+        
+        // Create a new modifier
+        ipcRenderer.send('createBeatmapModifier', {
+            'beatmapUrl': beatmapUrl,
+            'modifier': modifier
+        });
+
+        $(`#message_${thisId}`).html('Saved!');
+    }
+});
+
+$('#allModifiers').on('click', '[id^=remove_]', function() {
+    const thisId = this.id.replace('remove_', '');
+
+    // Delete a modifier
+    ipcRenderer.send('deleteModifier', thisId);
+});
