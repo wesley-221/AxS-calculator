bf5951147a6d0b93f233c9abdecf72e396f9562e
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-01-19 22:05:46 +0100

    Added a loading screen when booting

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const store = new Store();
 
 // ============================
 // Create some global variables
-let mainWindow;
+let mainWindow,
+    loadingWindow;
 
 initialize = async () => {
     const cache = store.get('cache');
@@ -36,7 +37,9 @@ initialize = async () => {
         const windowOptions = {
             title: app.getName(),
             icon: __dirname + '/assets/images/axs.ico',
-            frame: false            
+            frame: false,
+            titleBarStyle: 'hidden',
+            show: false
         };
 
         // Check for OS
@@ -47,15 +50,52 @@ initialize = async () => {
         mainWindow = new BrowserWindow(windowOptions);
         mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
 
-        mainWindow.maximize();
-
         mainWindow.on('closed', () => {
             mainWindow = null;
+        });
+
+        mainWindow.once('ready-to-show', () => {
+            loadingWindow.destroy();
+
+            mainWindow.show();
+            mainWindow.maximize();
+        });
+    }
+
+    createLoadingWindow = () => {
+        const windowOptions = {
+            title: app.getName(),
+            icon: __dirname + '/assets/images/axs.ico',
+            frame: false,
+            titleBarStyle: 'hidden',
+            show: false,
+            width: 960,
+            height: 540
+        };
+
+        // Check for OS
+        if(process.platform === 'linux') {
+            windowOptions.icon = path.join(__dirname, '/assets/images/axs.png');
+        }
+
+        loadingWindow = new BrowserWindow(windowOptions);
+        loadingWindow.loadURL(path.join('file://', __dirname, '/loading.html'));
+
+        loadingWindow.on('closed', () => {
+            loadingWindow = null;
+        });
+
+        loadingWindow.once('ready-to-show', () => {
+            loadingWindow.show();
         });
     }
 
     app.on('ready', () => {
-        createWindow();
+        createLoadingWindow();
+
+        loadingWindow.once('ready-to-show', () => {
+            createWindow();
+        })
     });
 
     app.on('window-all-closed', () => {
